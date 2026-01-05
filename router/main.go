ee3dd3f0425c298519101da80e0d62ffcad769f0
ShengNW <1916232522@qq.com>
2026-01-05 22:44:07 +0000

    recover router

---
+++ router/main.go
@@ -0,0 +1,40 @@
+package router
+
+import (
+	"embed"
+	"fmt"
+	"net/http"
+	"os"
+	"strings"
+
+	"github.com/gin-gonic/gin"
+
+	"github.com/yeying-community/router/common/config"
+	"github.com/yeying-community/router/common/logger"
+)
+
+func SetRouter(engine *gin.Engine, buildFS embed.FS) {
+	indexPage, err := buildFS.ReadFile("web/build/default/index.html")
+	if err != nil {
+		panic(err)
+	}
+
+	SetApiRouter(engine)
+	SetDashboardRouter(engine)
+	SetRelayRouter(engine)
+
+	frontendBaseURL := os.Getenv("FRONTEND_BASE_URL")
+	if config.IsMasterNode && frontendBaseURL != "" {
+		frontendBaseURL = ""
+		logger.SysLog("FRONTEND_BASE_URL is ignored on master node")
+	}
+	if frontendBaseURL == "" {
+		SetWebRouter(engine, buildFS, indexPage)
+		return
+	}
+
+	frontendBaseURL = strings.TrimSuffix(frontendBaseURL, "/")
+	engine.NoRoute(func(c *gin.Context) {
+		c.Redirect(http.StatusMovedPermanently, fmt.Sprintf("%s%s", frontendBaseURL, c.Request.RequestURI))
+	})
+}
